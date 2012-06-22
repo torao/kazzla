56c191dd9108dc8a36ef96acb0186485d60f2972
Takami Torao <koiroha@gmail.com>
2012-06-22 18:58:30 +0900

    modified

---
+++ src/main/scala/com/kazzla/drpc/Protocol.scala
@@ -0,0 +1,14 @@
+/* Copyright (C) 2012 BJöRFUAN
+ * This source and related resources are distributed under Apache License, Version 2.0.
+ */
+package com.kazzla.drpc
+
+// +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+// Protocol
+// +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+/**
+ * @author Takami Torao
+ */
+trait Protocol {
+
+}
