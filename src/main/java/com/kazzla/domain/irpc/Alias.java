3f78a21b5ea0a7e3866536339aa827f46fa2b308
Takami Torao <koiroha@gmail.com>
2013-05-17 15:28:21 +0900

    remove sources of project root

--- src/main/java/com/kazzla/domain/irpc/Alias.java
+++
@@ -1,6 +0,0 @@
-package com.kazzla.domain.irpc;/* Copyright (C) 2012 BJöRFUAN
- * This source and related resources are distributed under Apache License, Version 2.0.
- */
-
-public @interface Alias {
-}
